bdf1a86dd32aee88facee992290b6213ddfda503
luiscarneiro13 <carneiroluis2@gmail.com>
2022-09-01 16:52:22 -0400

    ...

--- app/Http/Controllers/API/V1/CategoriasController.php
+++ app/Http/Controllers/API/V1/CategoriasController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Repositories\CategoriesRepository;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
-
 class CategoriasController extends Controller
 {
     use ApiResponser;
