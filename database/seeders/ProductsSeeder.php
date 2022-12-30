491ddf48c7c40eb2becc111c091a0543868855b5
luiscarneiro13 <carneiroluis2@gmail.com>
2022-12-30 10:55:22 -0400

    ...

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -14,6 +14,6 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory()->count(100)->create();
+        Product::factory()->count(10)->create();
     }
 }
