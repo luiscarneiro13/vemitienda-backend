f4b78bc3f4083b5e4882e90d6d2d93e0712d76d9
Luis Carneiro <carneiroluis2@gmail.com>
2023-01-20 11:43:34 -0400

    ...

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -36,7 +36,6 @@
 
     <div class="row">
         @forelse ($catalog as $item)
-        {{ env('DO_URL_BASE').'/'.$item->image[0]->url }}
         <div class="col-md-4 p-1">
             <div class="text-center">
 
