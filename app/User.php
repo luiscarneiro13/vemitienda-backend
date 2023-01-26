875b11d0439da2e31dca131536f051d9cb71a0f3
Luis Carneiro <carneiroluis2@gmail.com>
2023-01-26 11:43:52 -0400

    ...

--- app/User.php
+++ app/User.php
@@ -3,14 +3,13 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, SoftDeletes, HasFactory;
+    use HasApiTokens, Notifiable, HasFactory;
 
     /**
      * The attributes that are mass assignable.
