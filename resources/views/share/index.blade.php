81762c04de6e3433f719d96d0f6ccd71203abd9e
Luis Carneiro <carneiroluis2@gmail.com>
2023-01-28 00:48:20 -0400

    Se agregan metadatos para compartir por whatsapp

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -5,7 +5,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <title>{{ $company->name }}</title>
+    <meta property="og:title" content="{{ $company->name }}" />
+    <meta name="description" content="{{ $company->slogan }}" />
+    <meta property="og:description" content="{{ $company->slogan }}" />
+    <meta property="og:image" content="{{ $company->logo->thumbnail }}" />
+    <meta property="og:type" content="article" />
+
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
