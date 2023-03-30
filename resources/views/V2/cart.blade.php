d9e3b8c94399960ff08099859ff78efaea0ee875
Luis Carneiro <carneiroluis2@gmail.com>
2023-03-29 20:46:37 -0400

    ...

--- resources/views/V2/cart.blade.php
+++ resources/views/V2/cart.blade.php
@@ -23,6 +23,20 @@
                     Resumen del Pedido
                 </h2>
             </div>
+
+            <div class="flex items-center p-2 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <a href="{{ url($slug) }}"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 ">
+                    Seguir comprando
+                </a>
+                @if (Cart::getTotalQuantity() > 0)
+                    <button data-modal-hide="defaultModal" type="button"
+                        class="close-modal text-white bg-{{ $company->theme->name }} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                        Finalizar pedido
+                    </button>
+                @endif
+            </div>
+
             @foreach ($cartItems as $item)
                 <x-cardCart :product="$item" :company="@$company" />
             @endforeach
@@ -50,18 +64,6 @@
             <div
                 class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b text-{{ $company->theme->name }}">
                 Total a pagar: $<span id="total">{{ Cart::getTotal() }}</span>
-            </div>
-            <div class="flex items-center p-2 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <a href="{{ url($slug) }}"
-                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 ">
-                    Seguir comprando
-                </a>
-                @if (Cart::getTotalQuantity() > 0)
-                    <button data-modal-hide="defaultModal" type="button"
-                        class="close-modal text-white bg-{{ $company->theme->name }} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                        Finalizar pedido
-                    </button>
-                @endif
             </div>
         </div>
 
