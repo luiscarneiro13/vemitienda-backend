dc20aa41faa0974b2780363dd8c21bf114892918
Luis Carneiro <carneiroluis2@gmail.com>
2023-04-04 14:55:35 -0400

    ...

--- app/Http/Controllers/API/V2/ImagesController.php
+++ app/Http/Controllers/API/V2/ImagesController.php
@@ -153,7 +153,7 @@ class ImagesController extends Controller
             try {
                 $urlImage = Images::uploadImage('images');
                 $thumbnail = Images::uploadThumbnail('thumbnails');
-                sleep(3);
+                // sleep(3);
                 $image = $product->image()->create(['url' => $urlImage, 'thumbnail' => $thumbnail]);
                 return $this->successResponse(['data' => $image]);
             } catch (Exception $th) {
@@ -224,7 +224,7 @@ class ImagesController extends Controller
             try {
                 $urlImage = Images::uploadImage('images');
                 $thumbnail = Images::uploadThumbnail('thumbnails');
-                sleep(3);
+                // sleep(3);
                 $product = Product::find($image->imageable_id);
                 $product->image()->delete();
                 $image = $product->image()->create(['url' => $urlImage, 'thumbnail' => $thumbnail]);
