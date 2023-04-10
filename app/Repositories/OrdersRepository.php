f89d4070f054310a6e847496bad4bcef66c55e09
Luis <carneiroluis2@gmail.com>
2023-04-10 19:31:14 -0400

    ...

--- app/Repositories/OrdersRepository.php
+++ app/Repositories/OrdersRepository.php
@@ -13,7 +13,7 @@ class OrdersRepository
         $user = Auth::user();
         $company = Company::where('user_id', $user->id)->first();
 
-        return Order::with('details')
+        return Order::with('details', 'status')
             ->where('company_id', $company->id)
             ->get();
     }
