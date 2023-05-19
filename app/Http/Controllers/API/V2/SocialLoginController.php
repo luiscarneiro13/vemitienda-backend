4dba7f117e9d513e96537ed92278bf98d4e56650
Luis Carneiro <carneiroluis2@gmail.com>
2023-05-19 16:44:39 -0400

    ...

--- app/Http/Controllers/API/V2/SocialLoginController.php
+++ app/Http/Controllers/API/V2/SocialLoginController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\API\V2;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class SocialLoginController extends Controller
 {
@@ -15,28 +18,55 @@ class SocialLoginController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
-    public function handleProviderCallback($provider)
+    public function handleProviderCallback(Request $request, $provider)
     {
         try {
             //code...
-            $socialUser = Socialite::driver($provider)->user();
+            $socialUser = Socialite::driver($provider)->stateless()->userFromToken($request->input('token'));
 
-            $user = User::where('provider_user_id', $socialUser->getId())->first();
+            $user = User::where('email', $socialUser->email)->first();
 
             if (!$user) {
                 $user = new User();
                 $user->name = $socialUser->getName();
                 $user->email = $socialUser->getEmail();
+                $user->password = Hash::make('lakjasldkj2348723kljhnsdf');
                 $user->provider = $provider;
                 $user->provider_user_id = $socialUser->getId();
                 $user->save();
             }
 
-            $user->token = $user->createToken('Social Login')->accessToken;
+            Auth::login($user, true);
+
+            $user->token = $user->createToken(env('APP_KEY'))->accessToken;
             $data = $user;
             return $this->successResponse(['data' => $data]);
         } catch (\Throwable $th) {
             return $this->errorResponse(['message' => $th]);
         }
     }
+
+    // public function handleProviderCallback($provider)
+    // {
+    //     try {
+    //         //code...
+    //         $socialUser = Socialite::driver($provider)->user();
+
+    //         $user = User::where('email', request()->email)->first();
+
+    //         if (!$user) {
+    //             $user = new User();
+    //             $user->name = $socialUser->getName();
+    //             $user->email = $socialUser->getEmail();
+    //             $user->password= Hash::make('sadlkjhASLKDJ23879287323');
+    //             $user->save();
+    //         }
+
+    //         $user->token = $user->createToken(env('APP_KEY'))->accessToken;
+    //         $data = $user;
+    //         return $this->successResponse(['data' => $data]);
+    //     } catch (\Throwable $th) {
+    //         return $this->errorResponse(['message' => $th]);
+    //     }
+    // }
 }
