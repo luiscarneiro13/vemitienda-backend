5a66092fd5ce67a7379676636b6a5a93a7f480d7
Luis <carneiroluis2@gmail.com>
2023-05-19 15:39:12 -0400

    ...

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ Route::group(['prefix' => 'v1'], function () {
 Route::group(['prefix' => 'v2'], function () {
 
     Route::group(['prefix' => 'social'], function () {
-        Route::post('/login/{provider}', [SocialLoginController::class, 'redirectToProvider']);
         Route::get('/login/{provider}/callback', [SocialLoginController::class, 'handleProviderCallback']);
     });
 
