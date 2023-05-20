24748ea5c0f5008cd5248945b5ed6faafff4db66
Luis <carneiroluis2@gmail.com>
2023-05-20 15:36:13 -0400

    ...

--- app/Http/Controllers/API/V2/SocialLoginController.php
+++ app/Http/Controllers/API/V2/SocialLoginController.php
@@ -8,51 +8,24 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
 
 class SocialLoginController extends Controller
 {
-
     use ApiResponser;
 
-    public function handleProviderCallback(Request $request, $provider)
+    public function handleProviderCallback(Request $request, $provider, User $userModel)
     {
-        try {
-
-            $token = request()->access_token;
-
-            $providerUser = Socialite::driver($provider)->userFromToken($token);
-
-            $user = User::where('provider_user_id', $providerUser->id)->first();
-
-            // Si no existe el proveedor dentro de ningún usuario
-            if (!$user) {
-
-                // Busco al usuario por su email
-                $user = User::where('email', request()->email)->first();
-
-                // Si lo encuentro, actualizo su perfil
-                if ($user) {
-                    $user->provider_user_id = $providerUser->id;
-                    $user->save();
-                } else { // Si no lo encuentro, creo el usuario
-                    $user = new User();
-                    $user->name = request()->name;
-                    $user->email = request()->email;
-                    $user->password = Hash::make('lkjasdlkj98729834oiHJHJAuiywermnqwe76');
-                    $user->provider_user_id = $providerUser->id;
-                    $user->save();
-                }
-            }
-
-            $user->token = $user->createToken(env('APP_KEY'))->accessToken;
-            $data = $user;
-            return $this->successResponse(['data' => $data]);
-        } catch (Exception $th) {
-            info($th);
-            return $this->errorResponse(['error' => $th]);
-        }
+        $providerUser = Socialite::driver($provider)->userFromToken($request->input('access_token'));
+        $user = $userModel->firstOrCreate(
+            ['provider_user_id' => $providerUser->id],
+            [
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make('lkjasdlkj98729834oiHJHJAuiywermnqwe76'),
+                'provider_user_id' => $providerUser->id,
+            ]
+        );
+        $user->token = $user->createToken(env('APP_KEY'))->accessToken;
+        return $this->successResponse(['data' => $user]);
     }
-
 }
