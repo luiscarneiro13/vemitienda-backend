09a55dc59478d17c6499c3b4739d618bcc7040db
Luis <carneiroluis2@gmail.com>
2023-05-20 13:44:38 -0400

    ...

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ use Illuminate\Support\Facades\Route;
 
 // Auth::routes();
 Route::get('/', 'NewHomeController@index')->name('home');
-
+Route::get('politica', function () {
+    return view('politica');
+});
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 
@@ -45,4 +47,3 @@ Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.up
 Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
 Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
 Route::post('order', [OrderController::class, 'index'])->name('order.store');
-
