58a4f49ee43816e8213d38f034bd36ee607d2614
Luis <carneiroluis2@gmail.com>
2023-08-20 12:13:19 -0400

    ...

--- app/Http/Controllers/WEB/V3/ProductController.php
+++ app/Http/Controllers/WEB/V3/ProductController.php
@@ -25,16 +25,14 @@ class ProductController extends Controller
         $id_usuario = $data['company']->user_id;
         $planUser = PlanUser::where('user_id', $id_usuario)->orderBy('id', 'Desc')->first();
         $data['categories'] = Category::where('user_id', $id_usuario)->get();
-        if ($planUser && $planUser->plan_id >= 2) {
+        $cat = 0;
+        $data['slug'] = $slug;
 
-            $cat = 0;
-            $data['slug'] = $slug;
+        if (request()->cat && request()->cat > 0) {
+            $cat = request()->cat;
+        }
 
-            if (request()->cat && request()->cat > 0) {
-                $cat = request()->cat;
-            }
-
-            $total = Product::query()
+        $total = Product::query()
             ->with('image', 'category')
             ->where('share', 1)
             ->where('user_id', $id_usuario)
@@ -43,9 +41,9 @@ class ProductController extends Controller
             })
             ->count();
 
-            $data['pages'] = (int)($total / 4);
+        $data['pages'] = (int)($total / 4);
 
-            $data['products'] = Product::query()
+        $data['products'] = Product::query()
             ->with('image', 'category')
             ->where('share', 1)
             ->where('user_id', $id_usuario)
@@ -53,10 +51,6 @@ class ProductController extends Controller
                 $q->where('category_id', request()->cat);
             })
             ->paginate(10);
-        } else {
-            $data['products'] = [];
-            $data['pages'] = -1;
-        }
 
         if ($request->ajax()) {
             $view = view('V3.data', $data)->render();
