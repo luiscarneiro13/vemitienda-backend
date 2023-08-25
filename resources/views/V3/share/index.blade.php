8072f94182110bf3715be0244612727981f8e242
Luis Carneiro <carneiroluis2@gmail.com>
2023-08-25 11:51:58 -0400

    ...

--- resources/views/V3/share/index.blade.php
+++ resources/views/V3/share/index.blade.php
@@ -3,6 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
+
+    <link rel="icon" type="image/x-icon" href="{{ env('DO_URL_BASE') . '/' . $company->logo->thumbnail }}">
+
     <meta name="description" content="Ogani Template">
     <meta name="keywords" content="Ogani, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
