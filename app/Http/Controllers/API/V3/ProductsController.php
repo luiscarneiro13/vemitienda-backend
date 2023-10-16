229711fec39cc27ed9077c00aad9a8350b694c1f
Luis Carneiro <luis.carneiro@territorium.com>
2023-10-16 13:11:07 -0400

    ...

--- app/Http/Controllers/API/V3/ProductsController.php
+++ app/Http/Controllers/API/V3/ProductsController.php
@@ -147,6 +147,7 @@ class ProductsController extends Controller
      */
     public function update(Request $request, $id)
     {
+        return $this->errorResponse(['status' => 400, 'message' => 'Error al actualizar']);
         try {
             return $this->successResponse(['data' =>  ProductsRepository::updateProduct($id)]);
         } catch (\Throwable $th) {
