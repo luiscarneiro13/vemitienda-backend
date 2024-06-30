01574bb1628dc88f9eba9d03144310fe49fdaa34
luiscarneiro13 <carneiroluis2@gmail.com>
2024-06-30 12:12:24 -0400

    ...

--- config/queue.php
+++ config/queue.php
@@ -86,7 +86,7 @@ return [
     // ],
 
     'failed' => [
-        'driver' => 'database-uuids',
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'table' => env('QUEUE_FAILED_TABLE', 'failed_jobs'),
         'database' => env('DB_CONNECTION', 'mysql'),
     ],
